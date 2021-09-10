3c688757b6c2ba532364090f511b28dd88559d4e
Adelson Guimarães <adelsonguimaraes@gmail.com>
2021-09-10 17:34:17 -0400

    iniciando o versionamento

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::page')
+
+@section('title', 'Painel')
+
+@section('content_header')
+    <h1>Painel de controle</h1>
+@endsection
+
+@section('content')
+    ...
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="./assets/css/custon.css">
+@endsection
+
+@section('js')
+    <script>
+        console.lot('rodando');
+    </script>
+@endsection
